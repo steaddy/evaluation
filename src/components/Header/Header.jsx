a82e9ceaadbf8260b7c4aa4f91f14f8cfec4cb2a
Steaddy <9265214314@mai.ru>
2022-11-03 07:55:59 +0300

    Put raw logo in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import {Button} from 'antd';
 import './Header.css';
 import {NavLink} from "react-router-dom";
+import logo from './logo.jpg';
 
 
 const Header = () => {
     return (
         <div className='header'>
+            <img className='logo' src={logo}/>
             <NavLink to='/'>
                 <Button type='primary' className="btn">Login</Button>
             </NavLink>
